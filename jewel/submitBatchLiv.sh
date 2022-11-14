3e6c2eb04843b0a0166a9e5f972c8719177fadd3
jaimenorman <jaime.norman@cern.ch>
2022-11-14 10:12:11 +0000

    add JEWEL files for running on batch

---
+++ jewel/submitBatchLiv.sh
@@ -0,0 +1,41 @@
+# submit job script to batch via qsub (slc6) or slurm (centos7)
+
+# Variables to control job loop
+# number to start at
+OFFSET=1
+# number of parallel jobs to run
+JOBS=2
+
+# Outdir labelled by date and time
+OUTDIR=/scratch/jnorman/JEWEL/$(date +%Y%m%d.%H%M)
+# job name - files will be output to this name
+MACRO='test'
+
+mkdir $OUTDIR
+
+# Work out automatically the OS
+SLURM=$([[ -f /etc/redhat-release ]] && cat /etc/redhat-release | grep -c CentOS)
+
+
+
+echo "Submitting $JOBS jobs with $MACRO ..."
+
+# Do a lop over the different jobs you want to run
+for ((i=$OFFSET; i< $OFFSET + $JOBS; i++))
+do
+    echo "... $i"
+    JOBNAME=$(echo $MACRO | cut -d '.' -f 1)".$i"
+    if [[ $SLURM -eq 1 ]] 
+    then
+	# Submit from CC7, seeting the queue (-p), output and error files (-o and -r), and shell vars to pass to the script running on the notes (--export)
+	# Check jobs with squeue
+	# Cancel jobs with scancel
+	sbatch -J $JOBNAME -p compute -o $OUTDIR/$JOBNAME.out -e $OUTDIR/$JOBNAME.err --export=MACRO=$MACRO,OUTDIR=$OUTDIR,SEED=$i runBatchLiv.sh
+    else
+	# Submit from SLC6, seeting the queue (-q), output and error files (-o and -r), and shell vars to pass to the script running on the notes (-v)
+	# Check jobs with qstat
+	# Cancel jobs with qdel
+	qsub -N $JOBNAME -q medium64 -o $OUTDIR/$JOBNAME.out -e $OUTDIR/$JOBNAME.err -v MACRO=$MACRO,OUTDIR=$OUTDIR,SEED=$i runBatchLiv.sh
+    fi    
+done
+
